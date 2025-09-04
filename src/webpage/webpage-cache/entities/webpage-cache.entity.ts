1c0030361aab73a0e0a422d4401e5d1f1dcb1284
Alan Reid <alandreid@hotmail.co.uk>
2025-09-04 14:23:09 +0200

    feat: define WebpageCache entity with properties for caching functionality

--- src/webpage/webpage-cache/entities/webpage-cache.entity.ts
+++ src/webpage/webpage-cache/entities/webpage-cache.entity.ts
@@ -1,1 +1,16 @@
-export class WebpageCache {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+Entity()
+export class WebpageCache {
+  @PrimaryGeneratedColumn('uuid')
+  string: string
+
+  @Column({default: false})
+  confirmed: boolean;
+
+  @Column({default: ""})
+  hash: string;
+
+  @Column({type: 'date'})
+  date: Date
+}
