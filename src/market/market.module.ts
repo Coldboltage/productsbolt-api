1af2a40ba4a9a77d60170558b1e81d291fd14f51
Coldboltage <alandreid@hotmail.co.uk>
2026-04-27 16:18:34 +0100

    feat: add imports for Product, Webpage, and WebpageSnapshot modules in MarketModule

--- src/market/market.module.ts
+++ src/market/market.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MarketService } from './market.service';
 import { MarketController } from './market.controller';
+import { ProductModule } from 'src/product/product.module';
+import { WebpageModule } from 'src/webpage/webpage.module';
+import { WebpageSnapshotModule } from 'src/webpage-snapshot/webpage-snapshot.module';
 
 @Module({
+  imports: [ProductModule, WebpageModule, WebpageSnapshotModule],
   controllers: [MarketController],
   providers: [MarketService],
 })
